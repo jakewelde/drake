fd7221e2994246910a8b7ec350792acd9c4e0c81
Gregory Izatt <gizatt@mit.edu>
2014-11-18 18:44:26 -0500

    Updating to current dev state

--- examples/CBSE/fallingBrickWithIMUAndInitSQP.m
+++ examples/CBSE/fallingBrickWithIMUAndInitSQP.m
@@ -10,7 +10,7 @@ plant = RigidBodyManipulator(fullfile(getDrakePath,'systems','plants','test','Fa
 warning(w);
 x0 = [0;0;2.0;0.1*randn(3,1); 0; 0; -2.0; 1; 0; 0];
 
-N=20; tf=2.0;
+N=10; tf=2.0;
 
 plant_ts = TimeSteppingRigidBodyManipulator(plant,tf/(N-1));
 feedback = FullStateFeedbackSensor();
@@ -52,11 +52,12 @@ for i=1:length(scale_sequence)
 %   snprint('snopt.out');
   
   % final conditions constraint
-  %prog = addStateConstraint(prog,ConstantConstraint(x0(1:12)),1);
-  prog = addStateConstraint(prog,ConstantConstraint(xf(1:12)),N);
-  
+  prog = addStateConstraint(prog,ConstantConstraint(x0(1:12)),1);
+  %prog = addStateConstraint(prog,ConstantConstraint(xf(1:12)),N);
+%   
   % Add in IMU constraints
   ts = getBreaks(xtraj_ts);
+  dt = (xtraj_ts.tspan(end) - xtraj_ts.tspan(1))/N;
   for n=1:N
     % -> given J_imu at x_n, J_imu(x_n) * (x_n+1 - x_n) + J0 = z_n+1
     imu_constraint = zeros(6, 24);
@@ -72,7 +73,27 @@ for i=1:length(scale_sequence)
     for j=1:length(x)
       add = add*0;
       add(j) = eps;
-      out_this = (imu.output(plant_ts.getManipulator,0,double(x), []) - out_norm)/eps;
+      out_this = (imu.output(plant_ts.getManipulator,0,double(x+add), []) - out_norm)/eps;
+      acc = out_this(8:10);
+      gyro   % Add in IMU constraints
+  ts = getBreaks(xtraj_ts);
+  dt = (xtraj_ts.tspan(end) - xtraj_ts.tspan(1))/N;
+  for n=1:N
+    % -> given J_imu at x_n, J_imu(x_n) * (x_n+1 - x_n) + J0 = z_n+1
+    imu_constraint = zeros(6, 24);
+    imu_equality = zeros(6, 1);
+    x = xtraj_ts.eval(ts(n));
+    ximu = x(13:end);
+    x = x(1:12);
+    % Numerical:
+    out_norm = imu.output(plant_ts.getManipulator,0,double(x), []);
+    out_num = zeros(6, length(x));
+    eps = 1E-6;
+    add = x*0;
+    for j=1:length(x)
+      add = add*0;
+      add(j) = eps;
+      out_this = (imu.output(plant_ts.getManipulator,0,double(x+add), []) - out_norm)/eps;
       acc = out_this(8:10);
       gyro = out_this(5:7);
       out_num(:, j) = [acc; gyro];
@@ -85,18 +106,41 @@ for i=1:length(scale_sequence)
     % J * (xn+1 - x_n) + zn = zn+1
     % i.e. j*xn+1 = zn+1 - J0 + J*x_n
     % Jac * x_n+1 - Jac * x_n
-    if (n~=1)
+    %if (n~=1)
       imu_constraint(:, 1:12) = -out;
       imu_constraint(:, 13:end) = out;
-    end
+   % end
     %imu_constraint(offset_inner+1:offset_inner+6, offset+1:offset+12) = -out;
     % ... J0 = z_n+1
     %imu_constraint(offset_inner+1:offset_inner+6, offset+13:offset+18) = eye(6);
-    imu_equality(1:3) = ximu(8:10) - acc;
-    imu_equality(4:6) = ximu(5:7) - gyro;
+    imu_equality(1:3) = (ximu(8:10) - acc);
+    imu_equality(4:6) = (ximu(5:7) - gyro);
     xind = (n-1)*12+1:(n+1)*12;
     prog = prog.addConstraint(LinearConstraint(imu_equality,imu_equality,imu_constraint), xind);
-  end
+ end
+= out_this(5:7);
+      out_num(:, j) = [acc; gyro];
+    end
+    acc = out_norm(8:10);
+    gyro = out_norm(5:7);
+    out = out_num;
+
+    % 1st-order taylor expansion of imu output at x_n.
+    % J * (xn+1 - x_n) + zn = zn+1
+    % i.e. j*xn+1 = zn+1 - J0 + J*x_n
+    % Jac * x_n+1 - Jac * x_n
+    %if (n~=1)
+      imu_constraint(:, 1:12) = -out;
+      imu_constraint(:, 13:end) = out;
+   % end
+    %imu_constraint(offset_inner+1:offset_inner+6, offset+1:offset+12) = -out;
+    % ... J0 = z_n+1
+    %imu_constraint(offset_inner+1:offset_inner+6, offset+13:offset+18) = eye(6);
+    imu_equality(1:3) = (ximu(8:10) - acc);
+    imu_equality(4:6) = (ximu(5:7) - gyro);
+    xind = (n-1)*12+1:(n+1)*12;
+    prog = prog.addConstraint(LinearConstraint(imu_equality,imu_equality,imu_constraint), xind);
+ end
   
   
   if i == 1,
