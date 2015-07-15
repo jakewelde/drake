d9651fd0962cdef5ab44e0f4862b400087bbfad2
Gregory Izatt <gizatt@mit.edu>
2015-07-15 18:25:21 -0400

    allow planeval to approximate contact forces if none are supplied needed for reactive recovery

--- examples/Atlas/+atlasControllers/AtlasPlanEval.m
+++ examples/Atlas/+atlasControllers/AtlasPlanEval.m
@@ -5,17 +5,20 @@ classdef AtlasPlanEval < PlanEval
 
   properties (Access = protected)
     robot
+    lcmgl;
   end
 
   methods
     function obj = AtlasPlanEval(r, varargin)
       obj = obj@PlanEval(varargin{:});
       obj.robot = r;
+      obj.lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'atlas');
     end
 
     function qp_input = getQPControllerInput(obj, t, x, contact_force_detected)
       if nargin < 4
         contact_force_detected = zeros(obj.robot.getNumBodies(), 1);
+        contact_force_detected([obj.robot.foot_body_id.right, obj.robot.foot_body_id.left]) = getFootContacts(obj.robot, x(1:obj.robot.getNumPositions));
       end
       plan = obj.getCurrentPlan(t, x);
       qp_input = plan.getQPControllerInput(t, x, contact_force_detected);
