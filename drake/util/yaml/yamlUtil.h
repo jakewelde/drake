9d81519352f8a02fcddbf882795770abab94bb11
Gregory Izatt <gizatt@mit.edu>
2016-03-08 17:32:01 -0500

    Accessors for utilities needed for DRC-side control.

--- drake/util/yaml/yamlUtil.h
+++ drake/util/yaml/yamlUtil.h
@@ -29,3 +29,5 @@ drake::eigen_aligned_std_map<std::string, QPControllerParams> loadAllParamSets(
 RobotPropertyCache parseKinematicTreeMetadata(
     const YAML::Node& metadata, const RigidBodyTree<double>& robot);
 KinematicModifications parseKinematicModifications(const YAML::Node& mods);
+JointNames parseRobotJointNames(const YAML::Node& joint_names,
+                                const RigidBodyTree& tree);
