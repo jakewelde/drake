0e7a6f832980f7ffab2c346c7fe75dd1b790eb7c
Gregory Izatt <gizatt@mit.edu>
2016-06-07 11:57:29 -0400

    Modified GSFit to estimate a 'background image'; will like look for more of a background-subtraction approach later.

--- drake/examples/GelsightPerception/runGSFit.m
+++ drake/examples/GelsightPerception/runGSFit.m
@@ -19,22 +19,32 @@ good_image_indices = [1, ...
 %Kernel ... ones indicate membership, zeros nonmembership.
 % Centered about middle row and column
 kernel_2D = ones(10,10);
-num_samps = 12000;
+num_samps = 1200;
 
 assert(sum(sum(kernel_2D==0 | kernel_2D==1)) == numel(kernel_2D)); 
 
-% allocate the regression matrix A
-A = zeros(num_samps * length(good_image_indices),numel(kernel_2D),3);
-b = zeros(num_samps * length(good_image_indices),1,3);
-
+As = {};
+bs = {};
+ref_imgs = {};
+depth_imgs = {};
+    
 for image_index=good_image_indices
+    % allocate the regression matrix A
+    A = zeros(num_samps * length(good_image_indices),numel(kernel_2D),3);
+    b = zeros(num_samps * length(good_image_indices),3);
+    
     ref_img_2D = imread(['cleanrgb/ref',int2str(image_index),'.png']);
     depth_img_2D = imread(['cleandepth/ref',int2str(image_index),'.png']);
-    ref_img_2D = imresize(ref_img_2D,.2);
-    depth_img_2D = imresize(depth_img_2D,.2);
-    
+    ref_img_2D = imresize(ref_img_2D,.03);
+    depth_img_2D = imresize(depth_img_2D,.03);
     ref_img_2D = double(ref_img_2D)/255;
     depth_img_2D = double(depth_img_2D)/255;
+    
+    % For gradient
+    %for color=1:3
+    %    depth_img_2D(:,:,color) = conv2(depth_img_2D(:,:,color), [0,-.25,0;.25,0,-.25;0,.25,0], 'same');
+    %end
+    
     
     disp(size(ref_img_2D))
     disp(size(depth_img_2D))
@@ -47,6 +57,9 @@ for image_index=good_image_indices
     %Make 1D version of images
     ref_img = reshape(ref_img_2D, [rows*cols, 3]);
     depth_img = reshape(depth_img_2D, [rows*cols, 3]);
+    
+    ref_imgs{image_index} = ref_img;
+    depth_imgs{image_index} = depth_img;
     
     %Make kernel vector corresponding to starting kernel off in top-left
     % of image.
@@ -87,7 +100,7 @@ for image_index=good_image_indices
         out = ref_img_2D(r,c,:);
         
         A(i,:,:) = in;
-        b(i,1,:) = out;
+        b(i,:) = out;
         
         %figure;
         %imshow(reshape(in, krows, kcols, 3));
@@ -97,50 +110,83 @@ for image_index=good_image_indices
     disp(numel(in));
     disp(numel(out));
     %scatter(sampd(:,1),sampd(:,2));
+
+    As{image_index}=A;
+    bs{image_index}=b;
     
+end
+
+%Construct full matrix for regression
+Aconv = vertcat(As{:});
+bconv = vertcat(bs{:});
+
+Aeye = repmat(eye(length(ref_imgs{1})),[length(good_image_indices),1,3]);
+beye = vertcat(ref_imgs{:});
+
+Afull = [];
+bfull = [];
+for color=1:3
+    Afull(:,:,color) = blkdiag(Aconv(:,:,color), Aeye(:,:,color));
+    bfull(:,color) = vertcat(bconv(:,color), beye(:,color));
+end
+
+disp(size(Afull));
+disp(size(bfull));
+disp(size(Aconv));
+disp(size(bconv));
+disp(size(Aeye));
+disp(size(beye));
+
+full_model_lin = zeros(size(Afull,2),3);
+for color=1:3
+    Amat = Afull(:,:,color);
+    bvec = bfull(:,color);
+
+    disp(size(Amat));
+    disp(size(bvec));
     
-    filter_lin = zeros(krows*kcols,3);
-    for color=1:3
-        Amat = A(:,:,color);
-        bvec = b(:,:,color);
-        
-        filter_lin(:,color) = lsqlin(Amat, bvec, Amat*0, bvec*0); %Amat \ bvec;
-    end
-        
-    filter_2D = reshape(filter_lin, krows, kcols, 3);
-    
-    %imshow(filter_2D); 
-    
-    %Reconstruct ref_img_2D from depth_img_2D
-    recon_2D = [];
-    for color=1:3
-        recon_color = conv2(depth_img_2D(:,:,color), filter_2D(:,:,color));
-        
-        recon_2D = cat(3, recon_2D, recon_color);
-    end
-    recon_2D = recon_2D(krows:krows+rows-1, kcols:kcols+cols-1, :);
-    
-    %recon_2D = (recon_2D - min(min(min(recon_2D)))) / (max(max(max(recon_2D))) - min(min(min(recon_2D))));
-    recon_2D = max(recon_2D, 0*recon_2D);
-    
-    figure
-    %subplot(1,3,1);
-    %imshow(depth_img_2D);
-    %subplot(1,3,2);
-    %imshow(ref_img_2D);
-    %subplot(1,3,3);
-    %imshow(recon_2D);
-    imshow(cat(1, depth_img_2D, ref_img_2D, recon_2D));
-    
+    full_model_lin(:,color) = Amat \ bvec; %lsqlin(Amat, bvec, Amat*0, bvec*0);
+end
+
+filter_lin = full_model_lin(1:(krows*kcols),:,:);
+filter_2D = reshape(filter_lin, krows, kcols, 3);
+filter_2D = imrotate(filter_2D, 180);
+
+base_lin = full_model_lin((krows*kcols)+1:size(full_model_lin,1),:,:);
+base_2D =reshape(base_lin, rows, cols, 3);
+
+%imshow(filter_2D); 
+
+%Reconstruct ref_img_2D from depth_img_2D
+recon_2D = [];
+for color=1:3
+    recon_color = conv2(depth_img_2D(:,:,color), filter_2D(:,:,color), 'same');
+
+    recon_2D = cat(3, recon_2D, recon_color);
+end
+% recon_2D = recon_2D(krows:krows+rows-1, kcols:kcols+cols-1, :);
+
+%recon_2D = (recon_2D - min(min(min(recon_2D)))) / (max(max(max(recon_2D))) - min(min(min(recon_2D))));
+recon_2D = max(recon_2D, 0*recon_2D);
+recon_2D = recon_2D + base_2D;
+
+figure
+%subplot(1,3,1);
+%imshow(depth_img_2D);
+%subplot(1,3,2);
+%imshow(ref_img_2D);
+%subplot(1,3,3);
+%imshow(recon_2D);
+imshow(cat(1, depth_img_2D, ref_img_2D, recon_2D));
+
 % 	figure;
 %   plot(filter_lin);
-    
-    %ref_img_2D = (ref_img_2D(:,:,1) + ref_img_2D(:,:,2) + ref_img_2D(:,:,3))/3;
-    %depth_img_2D = (depth_img_2D(:,:,1) + depth_img_2D(:,:,2) + depth_img_2D(:,:,3))/3;
-    
-    %ref_img = padarray(ref_img, [10 10], 0, 'both');
-    
-    %imshow(filter);
-end
+
+%ref_img_2D = (ref_img_2D(:,:,1) + ref_img_2D(:,:,2) + ref_img_2D(:,:,3))/3;
+%depth_img_2D = (depth_img_2D(:,:,1) + depth_img_2D(:,:,2) + depth_img_2D(:,:,3))/3;
+
+%ref_img = padarray(ref_img, [10 10], 0, 'both');
+
+%imshow(filter);
 
 end
