a0accb9561841fe5d0b8f8ad3aaba893115f2310
Gregory Izatt <gizatt@mit.edu>
2017-01-14 17:01:07 -0500

    Return knapsack test, add test names

--- drake/solvers/test/gurobi_test.cc
+++ drake/solvers/test/gurobi_test.cc
@@ -12,7 +12,7 @@ using Eigen::MatrixXd;
 namespace drake {
 namespace solvers {
 
-GTEST_TEST(GurobiTest, ) {
+GTEST_TEST(GurobiTest, StarBattle) {
   MathematicalProgram prog;
   auto B = prog.NewBinaryVariables(10, 10, "B");
 
@@ -63,5 +63,32 @@ GTEST_TEST(GurobiTest, ) {
   std::cout << prog.GetSolution(B) << std::endl;
 }
 
+GTEST_TEST(GurobiTest, Knapsack) {
+  MathematicalProgram prog;
+  auto B_knapsack = prog.NewBinaryVariables(10, "B_knapsack");
+
+  VectorXd knapsack_costs(10);
+  VectorXd knapsack_obj_coeffs(10);
+  knapsack_costs << 16, 16,  8,  8, 4, 4, 2, 2, 1, 1;
+  knapsack_obj_coeffs << 32, 32, 15, 15, 6, 6, 1, 1, 1, 1;
+  double total_budget = 33;
+  prog.AddLinearConstraint(knapsack_costs.transpose(), 0, total_budget, {B_knapsack});
+  prog.AddLinearCost(-1.0*knapsack_obj_coeffs, {B_knapsack});
+
+  GurobiSolver gurobi_solver;
+
+  prog.SetSolverOption("GUROBI", "PoolSolutions", 1024);
+  prog.SetSolverOption("GUROBI", "PoolGap", 0.10);
+  prog.SetSolverOption("GUROBI", "PoolSearchMode", 2);
+
+  prog.SetSolverOption("GUROBI", "LogToConsole", 1);
+  prog.SetSolverOption("GUROBI", "OutputFlag", 1);
+
+  gurobi_solver.Solve(prog);
+
+  printf("done!\n");
+}
+
 }  // namespace solvers
 }  // namespace drake
+
